7d191ef5cfca22d206455a4f47f304d1361c045f
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2011-05-30 20:34:30 +0200

    ALMATH: fix compilation
    Please do not push before having *try* to compile, guys !

--- almath/types/altransform.h
+++ almath/types/altransform.h
@@ -32,7 +32,8 @@ namespace AL {
       /**
       * DEFAULT CONSTRUCTOR: create an identity matrix
       * (null Transform does not exist)
-      */g Standards </A>
+      * g Standards </A>
+      */
       Transform(): r1_c1(1.0f), r1_c2(0.0f), r1_c3(0.0f), r1_c4(0.0f),
         r2_c1(0.0f), r2_c2(1.0f), r2_c3(0.0f), r2_c4(0.0f),
         r3_c1(0.0f), r3_c2(0.0f), r3_c3(1.0f), r3_c4(0.0f) {}
