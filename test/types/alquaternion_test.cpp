1bcce01928aef726e0f1c0c8415d730546851dea
ccollette <ccollette@aldebaran-robotics.com>
2011-12-01 11:37:46 +0100

    ALMATH: add quaternion test

--- test/types/alquaternion_test.cpp
+++ test/types/alquaternion_test.cpp
@@ -119,43 +119,63 @@ TEST(ALQuaternionTest, creation)
   EXPECT_TRUE(pQua1.isNear(pQua2, 0.0001f));
 
   // norm
+  // function norm
   pQua1 = AL::Math::Quaternion(1.0f, 2.0f, 3.0f, 4.0f);
   float normQua = pQua1.norm();
   EXPECT_NEAR(normQua, sqrtf(30), 0.0001f);
 
   // normalize
+  // function normalize
   pQua1 = AL::Math::Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
   ASSERT_THROW(AL::Math::normalize(pQua1), std::runtime_error);
 
-  // inverse
-
-  // fromAngleAndAxisRotation
-
   // toVector
+  pQua1 = AL::Math::Quaternion(1.0f, 2.0f, 3.0f, 4.0f);
+  std::vector<float> pQua1Vect = pQua1.toVector();
 
-  // function quaternionFromAngleAndAxisRotation
+  EXPECT_NEAR(pQua1Vect.at(0), 1.0f, 0.0001f);
+  EXPECT_NEAR(pQua1Vect.at(1), 2.0f, 0.0001f);
+  EXPECT_NEAR(pQua1Vect.at(2), 3.0f, 0.0001f);
+  EXPECT_NEAR(pQua1Vect.at(3), 4.0f, 0.0001f);
 
-  // function norm
-
-  // function normalize
-
-  // function quaternionInverse
 
   // function angleAndAxisRotationFromQuaternion
+  float pAngle = 0.0f;
+  float pAxeX = 0.0f;
+  float pAxeY = 0.0f;
+  float pAxeZ = 0.0f;
 
-
-//  pT1
-//  pT2
-//  pQua1
-//  pQua2
-//  pQua3
-
-  // operator *= with Quaternion
-  // operator *  with Quaternion
+  // TODO: fix
+//  float angleDes = 10.0f*AL::Math::TO_RAD;
+//  pQua1 = AL::Math::quaternionFromTransform(AL::Math::Transform::fromRotX(angleDes));
+//  AL::Math::angleAndAxisRotationFromQuaternion(pQua1, pAngle, pAxeX, pAxeY, pAxeZ);
+//  EXPECT_NEAR(pAngle, angleDes, 0.0001f);
+//  EXPECT_NEAR(pAxeX, 1.0f, 0.0001f);
+//  EXPECT_NEAR(pAxeY, 0.0f, 0.0001f);
+//  EXPECT_NEAR(pAxeZ, 0.0f, 0.0001f);
 
   unsigned int nbX = 10;
   unsigned int nbY = 10;
   unsigned int nbZ = 10;
+
+  // fromAngleAndAxisRotation
+  // function quaternionFromAngleAndAxisRotation
+  for (unsigned int i=0; i<nbX; i++)
+  {
+    float angle = ((float)i)/(float(nbX))*AL::Math::_2_PI_;
+
+    pQua1 = AL::Math::Quaternion::fromAngleAndAxisRotation(angle, 1.0f, 0.0f, 0.0f);
+    pT1   = AL::Math::Transform::fromRotX(angle);
+    EXPECT_TRUE(pQua1.isNear(AL::Math::quaternionFromTransform(pT1), 0.0001f));
+
+    pQua1 = AL::Math::Quaternion::fromAngleAndAxisRotation(angle, 0.0f, 1.0f, 0.0f);
+    pT1   = AL::Math::Transform::fromRotY(angle);
+    EXPECT_TRUE(pQua1.isNear(AL::Math::quaternionFromTransform(pT1), 0.0001f));
+
+    pQua1 = AL::Math::Quaternion::fromAngleAndAxisRotation(angle, 0.0f, 0.0f, 1.0f);
+    pT1   = AL::Math::Transform::fromRotZ(angle);
+    EXPECT_TRUE(pQua1.isNear(AL::Math::quaternionFromTransform(pT1), 0.0001f));
+  }
 
   for (unsigned int i=0; i<nbX; i++)
   {
@@ -181,7 +201,19 @@ TEST(ALQuaternionTest, creation)
         pQua2 = AL::Math::quaternionFromTransform(pT2);
         pQua3 = pQua1*pQua2;
 
+        // operator *= with Quaternion
+        // operator *  with Quaternion
         EXPECT_TRUE(pQua3.isNear(AL::Math::quaternionFromTransform(pT3), 0.0001f));
+
+        pT1 = AL::Math::Transform::fromRotX(angleX)*
+            AL::Math::Transform::fromRotY(angleY)*
+            AL::Math::Transform::fromRotZ(angleZ);
+
+        pQua1 = AL::Math::quaternionFromTransform(pT1);
+        pQua2 = pQua1.inverse();
+        // inverse
+        // function quaternionInverse
+        EXPECT_TRUE(pQua2.isNear(AL::Math::quaternionFromTransform(pT1.inverse()), 0.0001f));
 
         // if (!pQua3.isNear(AL::Math::quaternionFromTransform(pT3), 0.0001f))
         // {
