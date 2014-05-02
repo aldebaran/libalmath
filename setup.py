7a3cdc5f9360ac78d2a723d19c3f6f8273e90b0b
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-05-02 12:46:58 +0200

    Port to qipy
    Change-Id: I46562bfd004241582c51a7485410c0b3cbb76ca3

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+import os
+from setuptools import setup, find_packages
+
+setup(name="almath",
+      version="0.1",
+      description="libalmath bindings",
+      url="https://community.aldebaran-robotics.com/doc/",
+      py_modules=['almath'],
+)
