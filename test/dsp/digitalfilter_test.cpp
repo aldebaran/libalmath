4f2961118a93c310a3b1a619c363b323bf01877e
Sébastien Dalibard <sdalibard@aldebaran-robotics.com>
2014-06-04 16:13:01 +0200

    ALMath: switch from std::deque to boost::circular_buffer
    * Add dependency to boost
    
    Change-Id: Iee068b2c0fb532f0d8aa7ef45fbe317442d4e805

--- test/dsp/digitalfilter_test.cpp
+++ test/dsp/digitalfilter_test.cpp
@@ -49,3 +49,55 @@ TEST(DigitalFilter, testRecurence)
 
 }
 
+TEST(DigitalFilter, testWeightsY)
+{
+  std::vector<float> input;
+  input.push_back(1.0f);
+  input.push_back(-0.55f);
+  input.push_back(45.6f);
+  input.push_back(0.085f);
+  input.push_back(15.2f);
+
+  const unsigned int order = 2;
+
+  std::vector<float> weightX;
+  weightX.push_back(1.0f);
+  weightX.push_back(-1.0f);
+  weightX.push_back(-0.5f);
+
+  std::vector<float> weightY;
+  weightY.push_back(1.0f);
+  weightY.push_back(0.5f);
+
+  const float dcGain = 1.0f;
+
+  AL::Math::DSP::DigitalFilter filter = AL::Math::DSP::DigitalFilter();
+
+  filter.configureFilter(order, weightX, weightY, dcGain);
+
+  std::vector<float> outputVector;
+
+  for(unsigned int i=0; i < input.size(); ++i)
+  {
+    float output = 0.0f;
+    if (i<2)
+    {
+        output = filter.processFilter(input[i]);
+        EXPECT_NEAR(output, input[i], 0.00001f);
+    }
+    else
+    {
+        output = filter.processFilter(input[i]);
+        EXPECT_NEAR(
+              output,
+              input[i-2]*weightX[0] +
+              input[i-1]*weightX[1] +
+              input[i]*weightX[2] +
+              outputVector[i - 2]*weightY[0]+
+              outputVector[i - 1]*weightY[1]
+            , 0.00001f);
+    }
+    outputVector.push_back(output);
+  }
+
+}
