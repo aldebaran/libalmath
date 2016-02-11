35768b453e307f5725eb69c264d089f5e73f3555
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2016-02-11 17:27:54 +0100

    rework conversion to ros::Time
    let the user decide which clock he uses when setting ros::Time,
    instead of forcing qi::SystemClock
    
    Change-Id: I5fd5603ce4e667d8309f9c653c2f52896813f7a4

--- test/scenegraph/test_qirosmsg.cpp
+++ test/scenegraph/test_qirosmsg.cpp
@@ -4,13 +4,25 @@
 using namespace AL;
 
 TEST(QiRosMsg, toValidRosTime) {
-  using qiTime = qi::SystemClock::time_point;
   using AL::Math::toValidRosTime;
-  EXPECT_EQ(ros::TIME_MAX, toValidRosTime(qiTime::max()));
-  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qiTime::min()));
-  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qiTime()));
+  using qiTime = qi::Clock::time_point;
+  using qiSystemTime = qi::SystemClock::time_point;
+  EXPECT_EQ(ros::TIME_MAX, toValidRosTime(qi::Duration::max()));
+  EXPECT_EQ(ros::TIME_MAX, toValidRosTime(qiTime::max().time_since_epoch()));
+  EXPECT_EQ(ros::TIME_MAX,
+            toValidRosTime(qiSystemTime::max().time_since_epoch()));
+
+  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qi::Duration::min()));
+  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qiTime::min().time_since_epoch()));
+  EXPECT_EQ(ros::TIME_MIN,
+            toValidRosTime(qiSystemTime::min().time_since_epoch()));
+
+  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qi::Duration(0)));
+  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qi::Seconds(-1)));
+
+  EXPECT_EQ(ros::Time(1), toValidRosTime(qi::Seconds(1)));
   EXPECT_EQ(ros::Time(1, 1),
-            toValidRosTime(qiTime(qi::Seconds(1) + qi::NanoSeconds(1))));
-  EXPECT_EQ(ros::TIME_MIN, toValidRosTime(qiTime(qi::Seconds(-1))));
-  EXPECT_EQ(ros::Time(1), toValidRosTime(qiTime(qi::Seconds(1))));
+            toValidRosTime(qi::Seconds(1) + qi::NanoSeconds(1)));
+  EXPECT_EQ(ros::Time(0, 999999999),
+            toValidRosTime(qi::Seconds(1) - qi::NanoSeconds(1)));
 }
